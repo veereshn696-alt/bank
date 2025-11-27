fd71da44ec9b35147a0baebc1bc326287b01d0c5
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 15:45:40 +0530

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,13 @@
+import sys
+if len(sys.argv)!=3:
+ print("usage : pyhton script.py <init_bal> <dep_bal>")
+ init_bal=1000
+ dep_bal=200
+else :
+ script_name=sys.argv[0]
+ init_bal=sys.argv[1]
+ dep_bal=sys.argv[2]
+total=init_bal+dep_bal
+print("initial bal is ",init_bal)
+print("deposit bal is ",dep_bal)
+print("total is ",total)
