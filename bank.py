f805a5f94078113455ea4681c60ec20cbedb6f29
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 15:51:20 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -5,8 +5,8 @@ if len(sys.argv)!=3:
  dep_bal=200
 else :
  script_name=sys.argv[0]
- init_bal=sys.argv[1]
- dep_bal=sys.argv[2]
+ init_bal=int(sys.argv[1])
+ dep_bal=(sys.argv[2])
 total=init_bal+dep_bal
 print("initial bal is ",init_bal)
 print("deposit bal is ",dep_bal)
